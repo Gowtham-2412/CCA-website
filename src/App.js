a394007327e27acaa7b671531fdfa2b3f696e01d
Amardeep Ranjan <amardeepranjan911@gmail.com>
2023-08-22 20:43:31 +0530

    react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from 'react';
 import Home from './Pages/Home/Home';
 import Events from './Pages/Event/Events';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div>
-      <Home />
-      <Events />
-    </div>
-
-
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/events" element={<Events />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
