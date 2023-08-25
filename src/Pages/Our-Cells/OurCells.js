cacb3974d04d8a91a4d45931899bc6a42fdb3226
Aashirya04 <aachandaka@gmail.com>
2023-08-25 16:47:39 +0530

    OurCells updated

--- src/Pages/Our-Cells/OurCells.js
+++ src/Pages/Our-Cells/OurCells.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './OurCells.css';
+import coreimg from '../../Assets/Images/core-cell.png';
+import rndimg from '../../Assets/Images/rnd.jpg'
+import robocellimg from '../../Assets/Images/robo-cell.jpg'
+import ecellimg from '../../Assets/Images/e-cell.jpg'
+import wdctimg from '../../Assets/Images/wdct.jpg'
+
+
+
+
 
 
 export default function OurCells() {
     const cellData = [
-        { title: "Core Cell", class: "core-cell cell" },
-        { title: "WDCT", class: "wdct cell cell " },
-        { title: "E Cell", class: "robo-cell cell " },
-        { title: "Robo Cell", class: "e-cell cell " },
-        { title: "R&D Cell", class: "rd-cell cell " }
+        { title: "Core Cell", class: "core-cell cell", img: coreimg },
+        { title: "E Cell", class: "e-cell cell ", img: ecellimg },
+        { title: "R&D Cell", class: "rd-cell cell ", img: rndimg },
+        { title: "Robo Cell", class: "robo-cell cell ", img: robocellimg },
+        { title: "WDCT", class: "wdct cell cell ", img: wdctimg },
     ];
 
     return (
@@ -17,7 +26,7 @@ export default function OurCells() {
             <p className="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc faucibus, quam id hendrerit varius, lacus tellus cursus turpis, quis tempus ante odio vitae lectus. Nullam interdum lacus at nibh pulvinar efficitur. Praesent et justo nisl. Integer porta porta mollis.</p>
             <div className="cell-container">
                 {cellData.map((cell, index) => (
-                    <div className={cell.class} key={index} >
+                    <div className={cell.class} style={{ backgroundImage: `url(${cell.img})` }} key={index} >
                         <hr className='hr' />
                         <h3 className="cell-heading">{cell.title}</h3>
                     </div>
