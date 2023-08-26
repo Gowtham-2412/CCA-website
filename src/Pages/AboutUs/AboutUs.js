93c204666b8d3abad77c1b5a4b5a33f641e13e60
Aashirya04 <aachandaka@gmail.com>
2023-08-26 23:29:04 +0530

    AboutUs updated

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -8,12 +8,12 @@ export default function AboutUs() {
         <div className='container-fluid aboutuspage'>
             <div className='row'>
                 <div className='col-12 col-lg-6'>
-                    <div className='abt-image mb-5'>
+                    <div className='abt-image '>
 
                     </div>
                 </div>
                 <div className='col-12 col-lg-6'>
-                    <h1 className='main-h pb-4 pt-3'>About CCA!</h1>
+                    <h1 className='main-h pb-3 pt-3'>About CCA!</h1>
                     <p className='abt-para pb-3'>CCA, Centre for Cognitive Activities, is the focal point where convergence of all technical and scientific endeavours of the students materialises. This club is the revolution which bridges the gap between knowledge and application. Bulk of the extracurricular activities held in the college all the year round are organised by the CCA , with the objective of probing the dark recesses of human mind so that the grey cells are stimulated to create , conceptualise and evolve, triggering a rebellion of the new age mind against baseless conventions and meek acceptance.</p>
                     <div className='text-center text-md-left'>
                         <button className='exp-button'>
