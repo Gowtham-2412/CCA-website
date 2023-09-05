2271276548d690e005a8c6a96faf5cd3354a73f3
Masum <masumahmadraza@gmail.com>
2023-09-06 00:58:41 +0530

    added photos in arhn page

--- src/Pages/Aarohan/Aarohan.js
+++ src/Pages/Aarohan/Aarohan.js
@@ -8,6 +8,24 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
+import { WdctCornar,
+    decathalon,
+    GOR,
+    OSH,
+    MEC,
+    conjecture,
+    inspiratie,
+    redode,
+    techmela,
+    acsc,
+    ARHN1,
+    ARHN2,
+    ARHN3,
+    ARHN4,
+    ARHN5,
+    ARHN6,
+    ARHN7,
+    ARHN8 } from '../../Assets/Images';
 
 
 const Aarohan = () => {
@@ -67,32 +85,42 @@ const Aarohan = () => {
         >
             <SwiperSlide>
                 <div class="card">
-                    <img src={sample} alt="" />
+                    <img src={ARHN1} alt="" />
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div class="card">
-                    <img src={sample} alt="" />
+                    <img src={ARHN2} alt="" />
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div class="card">
-                    <img src={sample} alt="" />
+                    <img src={ARHN3} alt="" />
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div class="card">
-                    <img src={sample} alt="" />
+                    <img src={ARHN4} alt="" />
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div class="card">
-                    <img src={sample} alt="" />
+                    <img src={ARHN5} alt="" />
                 </div>
             </SwiperSlide>
             <SwiperSlide>
                 <div class="card">
-                    <img src={sample} alt="" />
+                    <img src={ARHN6} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div class="card">
+                    <img src={ARHN7} alt="" />
+                </div>
+            </SwiperSlide>
+            <SwiperSlide>
+                <div class="card">
+                    <img src={ARHN8} alt="" />
                 </div>
             </SwiperSlide>
         </Swiper>
@@ -111,51 +139,73 @@ const Aarohan = () => {
             responsive={responsive}
         >
                 <div class="card1">
-                    <img src={sample} alt="" />
+                    <img src={WdctCornar} alt="" />
                     <div className="card-content">
-                        <h3>wdct corner</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio pariatur minus, neque dolorem maxime?</p>
+                        <h3>WDCT CORNER</h3>
+                        <p>The only creativity is one's own. The creative and scabies mind begins where the regular mind ends.</p>
                     </div>
                 </div>
                 <div class="card1">
-                    <img src={sample} alt="" />
+                    <img src={decathalon} alt="" />
                     <div className="card-content">
-                        <h3>wdct corner</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio pariatur minus, neque dolorem maxime?</p>
+                        <h3>DECATHLON</h3>
+                        <p>Gear up and place yourself in the shoes of Rico from Starwars Troopers, who joins the military to fight against the alien bugs as this Aarohan 2023, Team Aavishkar brings you Decathlon</p>
                     </div>
                 </div>
                 <div class="card1">
-                    <img src={sample} alt="" />
+                    <img src={conjecture} alt="" />
                     <div className="card-content">
-                        <h3>wdct corner</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio pariatur minus, neque dolorem maxime?</p>
+                        <h3>CONJECTURE</h3>
+                        <p>Does your mind often wander off from reality, wondering what could be the possible outcomes of crazy scenarios that you construct in your mind? Do you long for an adventurous event?</p>
                     </div>
                 </div>
                 <div class="card1">
-                    <img src={sample} alt="" /><div className="card-content">
-                        <h3>wdct corner</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio pariatur minus, neque dolorem maxime?</p>
+                    <img src={GOR} alt="" /><div className="card-content">
+                        <h3>GAME OF RECRUITMENT</h3>
+                        <p>Game of Recruitment bestows the opportunity to test your analytical and reasoning skills, witness disturbing group discussions, and finally participate in one of the most interesting aspect of the game, the mind-numbing interview.</p>
                     </div>
                 </div>
                 <div class="card1">
-                    <img src={sample} alt="" />
+                    <img src={inspiratie} alt="" />
                     <div className="card-content">
-                        <h3>wdct corner</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio pariatur minus, neque dolorem maxime?</p>
+                        <h3>INSPIRATIE</h3>
+                        <p>We are proud to host him at Inspiratie 2023.
+                        Don’t miss your opportunity to interact with him on 10th March, Inspiratie 2023.</p>
                     </div>
                 </div>
                 <div class="card1">
-                    <img src={sample} alt="" />
+                    <img src={acsc} alt="" />
                     <div className="card-content">
-                        <h3>wdct corner</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio pariatur minus, neque dolorem maxime?</p>
+                        <h3>ANNUAL CASE STUDY COMPITION</h3>
+                        <p>Competing is the only way to reach greatness and AAROHAN'23 is providing you with the perfect opportunity to showcase your business acumen and analytical skills.</p>
                     </div>
                 </div>
                 <div class="card1">
-                    <img src={sample} alt="" />
+                    <img src={techmela} alt="" />
                     <div className="card-content">
-                        <h3>wdct corner</h3>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa odio pariatur minus, neque dolorem maxime?</p>
+                        <h3>TECHMELA</h3>
+                        <p>Technology has the capability to change the world for the better. Imagination in people’s minds makes it possible to create something which can have wide ranging implications on the society.</p>
+                    </div>
+                </div>
+                <div class="card1">
+                    <img src={OSH} alt="" />
+                    <div className="card-content">
+                        <h3>ON SPOT HACKATON</h3>
+                        <p>On Spot Hackathon 2023, is here to provide you the platform to showcase your skills and build working prototypes</p>
+                    </div>
+                </div>
+                <div class="card1">
+                    <img src={MEC} alt="" />
+                    <div className="card-content">
+                        <h3>MAIDAN-E-CREATIVE</h3>
+                        <p>Web, Design and Creative Team, CCA presents Maidan-e-Creative , a week full of imagination and creativity.
+Learn how to bring your creations alive using Adobe Illustrator and Adobe Photoshop.</p>
+                    </div>
+                </div><div class="card1">
+                    <img src={redode} alt="" />
+                    <div className="card-content">
+                        <h3>RED ODYSSEY</h3>
+                        <p>Embark on the Mars quest expendition and design a rover to conquor treocherous rugged terrains and obstacles</p>
                     </div>
                 </div>
         </Carousel>
