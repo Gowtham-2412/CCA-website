282c5f40b8df4b7c051ad197705d0bb27179e344
Aashirya04 <aachandaka@gmail.com>
2023-09-25 00:39:07 +0530

    Footer css module updated

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Footer.css';
+import Footercss from './Footer.module.css';
 import instagramicon from '../../Assets/Icons/instagram.svg';
 import facebookicon from '../../Assets/Icons/facebook.svg';
 import twittericon from '../../Assets/Icons/twitter.svg';
@@ -9,20 +9,20 @@ import linkedinicon from '../../Assets/Icons/linkedin.svg';
 
 const Footer = () => {
   return (
-    <footer className="Footer">
-        <div className="socialmedia-icons">
-        <p className='footer-p'>Follow us:</p>
+    <footer className={Footercss.Footer}>
+        <div className={Footercss.socialmediaicons}>
+        <p className={Footercss.footerp}>Follow us:</p>
         <a href="https://www.instagram.com/cca.nitd/" target="_blank">
-         <img src={instagramicon} alt="Instagram" className="icon" />
+         <img src={instagramicon} alt="Instagram" className={Footercss.icon} />
          </a>
          <a href="https://www.facebook.com/ccanitd.in" target="_blank">
-          <img src={facebookicon} alt="Facebook" className="icon" />
+          <img src={facebookicon} alt="Facebook" className={Footercss.icon} />
           </a>
           <a href="https://twitter.com/aarohan_nitdgp" target="_blank">
-          <img src={twittericon} alt="Twitter" className="icon" />
+          <img src={twittericon} alt="Twitter" className={Footercss.icon} />
           </a>
           <a href="https://www.linkedin.com/company/center-for-cognitive-activities-nit-durgapur/" target="_blank">
-          <img src={linkedinicon} alt="LinkedIn" className="icon" />
+          <img src={linkedinicon} alt="LinkedIn" className={Footercss.icon} />
           </a>
       </div>
     </footer>
