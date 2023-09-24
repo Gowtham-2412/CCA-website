0e049a5ef93cd8e0e81f27513f883cafb9002274
Prasanthisandaka2005 <prasanthisandaka2005@gmail.com>
2023-09-25 00:19:08 +0530

    OurCells,Aboutus css modules updated

--- src/Pages/AboutUs/AboutUs.js
+++ src/Pages/AboutUs/AboutUs.js
@@ -1,24 +1,26 @@
 import React from 'react'
-import './AboutUs.css';
+import AboutCss from './AboutUs.module.css';
 import arrow from '../../Assets/Icons/arrow.png';
 
 
 export default function AboutUs() {
     return (
-        <div className='container-fluid aboutuspage'>
-            <div className='row'>
-                <div className='col-12 col-lg-6'>
-                    <div className='abt-image '>
+        <div className='container-fluid'>
+            <div className={AboutCss.aboutuspage}>
+                <div className='row'>
+                    <div className='col-12 col-lg-6'>
+                        <div className={AboutCss.abtimage}>
 
+                        </div>
                     </div>
-                </div>
-                <div className='col-12 col-lg-6'>
-                    <h1 className='main-h pb-3 pt-3'>About CCA!</h1>
-                    <p className='abt-para pb-3'>CCA, Centre for Cognitive Activities, is the focal point where convergence of all technical and scientific endeavours of the students materialises. This club is the revolution which bridges the gap between knowledge and application. Bulk of the extracurricular activities held in the college all the year round are organised by the CCA , with the objective of probing the dark recesses of human mind so that the grey cells are stimulated to create , conceptualise and evolve, triggering a rebellion of the new age mind against baseless conventions and meek acceptance.</p>
-                    <div className='text-center text-md-left'>
-                        <button className='exp-button'>
-                            Explore<img src={arrow} alt='' className='arrow-icon ml-3 mb-1' />
-                        </button>
+                    <div className='col-12 col-lg-6'>
+                        <h1 className={AboutCss.mainh}>About CCA!</h1>
+                        <p className={AboutCss.abtpara}>CCA, Centre for Cognitive Activities, is the focal point where convergence of all technical and scientific endeavours of the students materialises. This club is the revolution which bridges the gap between knowledge and application. Bulk of the extracurricular activities held in the college all the year round are organised by the CCA , with the objective of probing the dark recesses of human mind so that the grey cells are stimulated to create , conceptualise and evolve, triggering a rebellion of the new age mind against baseless conventions and meek acceptance.</p>
+                        <div className='text-center text-md-left'>
+                            <button className={AboutCss.expbutton}>
+                                Explore<img src={arrow} alt='explore-icon' className={AboutCss.arrowicon} />
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/Pages/Our-Cells/OurCells.js
+++ src/Pages/Our-Cells/OurCells.js
@@ -1,34 +1,29 @@
 import React from 'react';
-import './OurCells.css';
+import CellsCss from './OurCells.module.css';
 import coreimg from '../../Assets/Images/core-cell.png';
-import rndimg from '../../Assets/Images/rnd.jpg'
-import robocellimg from '../../Assets/Images/robo-cell.jpg'
-import ecellimg from '../../Assets/Images/e-cell.jpg'
-import wdctimg from '../../Assets/Images/wdct.jpg'
-
-
-
-
-
+import rndimg from '../../Assets/Images/rnd.jpg';
+import robocellimg from '../../Assets/Images/robo-cell.jpg';
+import ecellimg from '../../Assets/Images/e-cell.jpg';
+import wdctimg from '../../Assets/Images/wdct.jpg';
 
 export default function OurCells() {
     const cellData = [
-        { title: "Core Cell", class: "core-cell cell", img: coreimg },
-        { title: "E Cell", class: "e-cell cell ", img: ecellimg },
-        { title: "R&D Cell", class: "rd-cell cell ", img: rndimg },
-        { title: "Robo Cell", class: "robo-cell cell ", img: robocellimg },
-        { title: "WDCT", class: "wdct cell cell ", img: wdctimg },
+        { title: "Core Cell", class: CellsCss.corecell, img: coreimg },
+        { title: "E Cell", class: CellsCss.ecell, img: ecellimg },
+        { title: "R&D Cell", class: CellsCss.rdcell, img: rndimg },
+        { title: "Robo Cell", class: CellsCss.robocell, img: robocellimg },
+        { title: "WDCT", class: CellsCss.wdct, img: wdctimg },
     ];
 
     return (
-        <div className="our-cells-page">
-            <h1 className="main-heading pb-3">Our Cells</h1>
-            <p className="para">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc faucibus, quam id hendrerit varius, lacus tellus cursus turpis, quis tempus ante odio vitae lectus. Nullam interdum lacus at nibh pulvinar efficitur. Praesent et justo nisl. Integer porta porta mollis.</p>
-            <div className="cell-container">
+        <div className={CellsCss.ourcellspage}>
+            <h1 className={CellsCss.mainheading}>Our Cells</h1>
+            <p className={CellsCss.para}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc faucibus, quam id hendrerit varius, lacus tellus cursus turpis, quis tempus ante odio vitae lectus. Nullam interdum lacus at nibh pulvinar efficitur. Praesent et justo nisl. Integer porta porta mollis.</p>
+            <div className={CellsCss.cellcontainer}>
                 {cellData.map((cell, index) => (
-                    <div className={cell.class} style={{ backgroundImage: `url(${cell.img})` }} key={index} >
-                        <hr className='hr' />
-                        <h3 className="cell-heading">{cell.title}</h3>
+                    <div className={`${CellsCss.cell} ${cell.class}`} style={{ backgroundImage: `url(${cell.img})` }} key={index}>
+                        <hr className={CellsCss.hr} />
+                        <h3 className={CellsCss.cellheading}>{cell.title}</h3>
                     </div>
                 ))}
             </div>
