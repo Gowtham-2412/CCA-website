04060666584aa623128d5da1d091ef0182a2ffba
Obaidullah Azad <azadobaidullah19@gmail.com>
2023-10-08 16:30:51 +0530

    Hover added in hall

--- src/Pages/HallofFame/Hall.js
+++ src/Pages/HallofFame/Hall.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useRef } from 'react';
 import Hallcss from './Hall.module.css';
 import ImgLib from '../../Components/ImgLink/ImageLib';
+import instagram from '../../Assets/Icons/instagram.svg'
+import linkedin from '../../Assets/Icons/linkedin.svg'
 
 const Hall = () => {
   const [items, setItems] = useState(ImgLib);
@@ -54,12 +56,20 @@ const Hall = () => {
           const { name, image } = elem;
 
           return (
-            <div className={Hallcss.Cardsection}>
-              <div className={Hallcss.cardbody}>
+            <div className={Hallcss.Cardbody}>
                 <img src={image} alt={name}></img>
                 {console.log(image)}
+              <div className={Hallcss.cardcontent}>
+                <h1>SOMEONE NAME</h1>
+                <p>SENIOR Member</p>
               </div>
-            </div>
+              <div className={Hallcss.cardback}>
+              </div>
+              <div className={Hallcss.cardsocial}>
+                <a href='#'><img className = {Hallcss.socialicon} src={instagram}></img></a>
+                <a href='#'><img className = {Hallcss.socialicon} src={linkedin}></img></a>
+              </div>
+              </div>
           );
         })}
       </main>
